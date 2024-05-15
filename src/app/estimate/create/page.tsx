6ff2a511beed6dcd24cda510383acd9f441e50c2
Mahmud Nabil <nabilsnigdho@gmail.com>
2024-05-16 05:07:51 +0600

    feat: estimate tabs

--- src/app/estimate/create/page.tsx
+++ src/app/estimate/create/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/app/auth";
 import { SelectClient } from "@/components/Lists/SelectClient";
 import { SelectVehicle } from "@/components/Lists/SelectVehicle";
 import { SyncLists } from "@/components/SyncLists";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/Tabs";
 import Title from "@/components/Title";
 import { db } from "@/lib/db";
 import { AuthSession } from "@/types/auth";
@@ -14,10 +15,10 @@ export default async function Page() {
   const vehicles = await db.vehicle.findMany({ where: { companyId } });
 
   return (
-    <>
+    <div className="grid gap-3 md:grid-cols-[1fr,24rem]">
       <Title>Estimate</Title>
       <SyncLists customers={customers} vehicles={vehicles} />
-      <div className="app-shadow mt-5 flex flex-wrap items-center gap-3 rounded-md p-3">
+      <div className="app-shadow col-start-1 mt-5 flex flex-wrap items-center gap-3 rounded-md p-3">
         <div className="mr-auto">
           434534674576:{" "}
           <input
@@ -28,11 +29,24 @@ export default async function Page() {
           />
         </div>
         <CreateEstimateActionsButtons />
-        <div className="basis-full flex flex-wrap items-center gap-3">
+        <div className="flex basis-full flex-wrap items-center gap-3">
           <SelectClient />
           <SelectVehicle />
         </div>
       </div>
-    </>
+      <Tabs defaultValue="create" className="col-start-1">
+        <TabsList>
+          <TabsTrigger value="payments">Payments</TabsTrigger>
+          <TabsTrigger value="inspections">Inspections</TabsTrigger>
+          <TabsTrigger value="attachment">Attachment</TabsTrigger>
+          <TabsTrigger value="create">Create</TabsTrigger>
+        </TabsList>
+        <TabsContent value="create">
+        </TabsContent>
+        <TabsContent value="attachment"></TabsContent>
+        <TabsContent value="inspections"></TabsContent>
+        <TabsContent value="payments"></TabsContent>
+      </Tabs>
+    </div>
   );
 }
