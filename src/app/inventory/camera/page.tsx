caf438a6ef1e41c7b48b10164785209184a2a940
KR Shanto <krshanto2005@gmail.com>
2024-06-19 15:42:51 +0600

    ignore ts errors

--- src/app/inventory/camera/page.tsx
+++ src/app/inventory/camera/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import adapter from "webrtc-adapter";
-
 import { useRouter } from "next/navigation";
 import React, { useRef, useState } from "react";
 import { QrReader } from "react-qr-reader";
@@ -10,9 +8,9 @@ import { QrReader } from "react-qr-reader";
 export default function Page() {
   const [result, setResult] = useState<any>(null);
   const router = useRouter();
-  const qrRef = useRef(null);
+  const qrRef: any = useRef(null);
 
-  const handleScan = (result, error) => {
+  const handleScan = (result: any, error: any) => {
     if (!!result) {
       setResult(result?.text);
       router.push(result?.text);
@@ -36,6 +34,7 @@ export default function Page() {
             className="h-[300px] w-[300px] lg:h-[400px] lg:w-[400px]"
             onResult={handleScan}
             constraints={{ facingMode: "environment" }}
+            // @ts-ignore
             style={{ width: "40%", height: "40%" }}
             ref={qrRef}
           />
