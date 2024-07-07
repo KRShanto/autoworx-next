d0e5edd8c45cd5f16b961f29315daa1cbe2d3535
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-07-07 22:31:10 +0600

    Employee details and attendance first update

---
+++ src/app/employee/information/components/Payout.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PayoutCard from './PayoutCard'
+
+export default function Payout() {
+  return (
+    <div className='flex space-x-6 '>
+      <PayoutCard
+        title="Previous Month Payout"
+        amount="$3464"
+        percentage="100%"
+       
+        
+      />
+      <PayoutCard
+        title="Current Month Payout"
+        amount="$2780"
+        percentage="90%"
+        
+      />
+      <PayoutCard
+        title="YTD Payout"
+        amount="$4000"
+        percentage="85%"
+       
+      />
+    </div>
+  )
+}
