0f6cc269c7f599eeb9b889e8e9c9456b4599fc6a
KR Shanto <krshanto2005@gmail.com>
2024-07-29 18:00:34 +0600

    feat: fetch and display client data in `/client` page

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -10,185 +10,22 @@ import { IoPieChartOutline, IoSearchOutline } from "react-icons/io5";
 
 import AddNewClient from "./AddNewClient";
 import EditClient from "./EditClient";
+import { db } from "@/lib/db";
+import { getCompanyId } from "@/lib/companyId";
+
 const evenColor = "bg-white";
 const oddColor = "bg-slate-100";
-const clients = [
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-  {
-    id: 1234567890,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    phone: "+1234567890",
-  },
-];
-export default function Page({ params }: { params: { clientId: string } }) {
+
+export default async function Page() {
+  const companyId = await getCompanyId();
+  const clients = await db.customer.findMany({ where: { companyId } });
+
   return (
     <div className="h-full w-full space-y-8">
       <Title>Client List</Title>
 
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-8">
+        <div className="flex items-center gap-x-8 bg-white">
           <div className="flex w-[500px] items-center gap-x-2 rounded-md border border-gray-300 px-4 py-1 text-gray-400">
             <span className="">
               <IoSearchOutline />
@@ -196,7 +33,7 @@ export default function Page({ params }: { params: { clientId: string } }) {
             <input
               name="search"
               type="text"
-              className="w-full rounded-md px-4 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full rounded-md border border-slate-400 px-4 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Search"
             />
           </div>
@@ -204,55 +41,58 @@ export default function Page({ params }: { params: { clientId: string } }) {
         <AddNewClient />
       </div>
       <div>
-        <table className="w-full shadow-md">
-          <thead className="bg-white">
-            <tr className="h-10 border-b">
-              <th className="border-b px-4 py-2 text-center">Client ID</th>
-              <th className="border-b px-4 py-2 text-center">Client </th>
-              <th className="border-b px-4 py-2 text-center">Email</th>
-              <th className="border-b px-4 py-2 text-center">Phone</th>
-              <th className="border-b px-4 py-2 text-center">Edit</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {clients.map((client, index) => (
-              <tr
-                key={index}
-                className={cn(
-                  "cursor-pointer rounded-md py-3",
-                  index % 2 === 0 ? "bg-white" : "bg-blue-100",
-
-                )}
-
-              >
-                <td className="border-b px-4 py-2 text-center">
-                  <Link className="text-blue-500" href={`/client/${client.id}`}>
-                    {client.id}
-                  </Link>
-                </td>
-                <td className="border-b px-4 py-2 text-center">
-                  {client.name}
-                </td>
-                <td className="border-b px-4 py-2 text-center">
-                  {client.email}
-                </td>
-                <td className="border-b px-4 py-2 text-center">
-                  {client.phone}
-                </td>
-                <td className="border-b border-l bg-white px-4 py-2 text-center">
-                  <div className="flex items-center justify-center gap-2">
-                    <EditClient />
-
-                    <button className="text-xl text-red-400">
-                      <FaTimes />
-                    </button>
-                  </div>
-                </td>
+        <div className="app-shadow w-full rounded-lg bg-white p-3">
+          <table className="w-full">
+            <thead className="">
+              <tr className="h-10 border-b">
+                <th className="border-b px-4 py-2 text-left">Client ID</th>
+                <th className="border-b px-4 py-2 text-left">Client </th>
+                <th className="border-b px-4 py-2 text-left">Email</th>
+                <th className="border-b px-4 py-2 text-left">Phone</th>
+                <th className="border-b px-4 py-2 text-center">Edit</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+
+            <tbody>
+              {clients.map((client, index) => (
+                <tr
+                  key={index}
+                  className={cn(
+                    "py-3",
+                    index % 2 === 0 ? "bg-white" : "bg-[#EEF4FF]",
+                  )}
+                >
+                  <td className="border-b px-4 py-2 text-left">
+                    <Link
+                      className="text-blue-500"
+                      href={`/client/${client.id}`}
+                    >
+                      {client.id}
+                    </Link>
+                  </td>
+                  <td className="border-b px-4 py-2 text-left">
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td className="border-b px-4 py-2 text-left">
+                    {client.email}
+                  </td>
+                  <td className="border-b px-4 py-2 text-left">
+                    {client.mobile}
+                  </td>
+                  <td className="border-b border-l bg-white px-4 py-2 text-center">
+                    <div className="flex items-center justify-center gap-2">
+                      <EditClient />
+
+                      <button className="text-xl text-red-400">
+                        <FaTimes />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
