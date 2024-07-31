d09e6cf14e3326f6dbdef1b0399efb16fa40ea35
KR Shanto <krshanto2005@gmail.com>
2024-07-31 15:57:56 +0600

    remove `report` page

--- src/app/report/(reporting)/payments/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PaymentsPage(){
-    return (
-        <div>
-            Payments Page
-        </div>
-    );
-}
--- src/app/report/(reporting)/pipeline/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PipelinePage(){
-    return (
-        <div>
-            Pipeline Page
-        </div>
-    );
-}
