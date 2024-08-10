03aa2233c39a91da0a5663d9b4bb6fb57d69a72d
KR Shanto <krshanto2005@gmail.com>
2024-08-10 08:09:57 +0600

    move payment `Table` component into `PaymentTable` file.

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -7,15 +7,13 @@ import {
   TabsList,
   TabsTrigger,
 } from "./components/PaymentTab";
-import Link from "next/link";
 import HeaderSearch from "./components/HeaderSearch";
 import LogoCard from "./components/LogoCard";
 import CuponComponent from "./components/CuponComponent";
-import { ReturnPayment, getPayments } from "@/actions/payment/getPayments";
+import { getPayments } from "@/actions/payment/getPayments";
 import { useServerGet } from "@/hooks/useServerGet";
-import moment from "moment";
 import { getCoupons } from "@/actions/coupon/getCoupons";
-import { usePaymentFilterStore } from "@/stores/paymentFilter";
+import PaymentTable from "./components/PaymentTable";
 
 export default function Page() {
   const { data: payments } = useServerGet(getPayments);
@@ -43,7 +41,7 @@ export default function Page() {
         </TabsList>
 
         <TabsContent value="transactions">
-          <Table data={payments || []} />
+          <PaymentTable data={payments || []} />
         </TabsContent>
 
         <TabsContent value="integrations">
@@ -66,117 +64,6 @@ export default function Page() {
           <CuponComponent coupons={coupons || []} setCoupons={setCoupons} />
         </TabsContent>
       </PaymentTab>
-    </div>
-  );
-}
-
-function Table({ data }: { data: ReturnPayment[] }) {
-  const { search, dateRange, amount, paidStatus, paymentMethod } =
-    usePaymentFilterStore();
-  const [filteredData, setFilteredData] = useState(data);
-
-  function checkPaymentMethod(method: string) {
-    if (paymentMethod === "All") {
-      return true;
-    } else if (method === paymentMethod) {
-      return true;
-    } else if (
-      paymentMethod === "Other" &&
-      method !== "Card" &&
-      method !== "Cash" &&
-      method !== "Cheque"
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  useEffect(() => {
-    setFilteredData(
-      data.filter((item) => {
-        const isWithinDateRange =
-          dateRange[0] && dateRange[1]
-            ? moment(item.date).isSameOrAfter(dateRange[0], "day") &&
-              moment(item.date).isSameOrBefore(dateRange[1], "day")
-            : true;
-
-        const isWithinAmountRange =
-          item.amount >= amount[0] && item.amount <= amount[1];
-
-        const isPaymentMethodMatch = checkPaymentMethod(item.method);
-
-        const isPaidStatusMatch =
-          paidStatus === "All"
-            ? true
-            : paidStatus === "Paid"
-              ? item.paid
-              : !item.paid;
-
-        const isSearchMatch = search
-          ? item.vehicle?.toLowerCase().includes(search.toLowerCase()) ||
-            item.invoiceId.toLowerCase().includes(search.toLowerCase()) ||
-            item.client.name?.toLowerCase().includes(search.toLowerCase())
-          : true;
-
-        return (
-          isWithinDateRange &&
-          isWithinAmountRange &&
-          isPaymentMethodMatch &&
-          isPaidStatusMatch &&
-          isSearchMatch
-        );
-      }),
-    );
-  }, [data, dateRange, amount, paidStatus, paymentMethod, search]);
-
-  return (
-    <div className="min-h-[65vh] overflow-x-scroll rounded-md bg-white xl:overflow-hidden">
-      <table className="w-full">
-        {/*  Header */}
-        <thead className="bg-white">
-          <tr className="h-10 border-b">
-            <th className="border-b px-4 py-2 text-left">Invoice#</th>
-            <th className="border-b px-4 py-2 text-left">Customer</th>
-            <th className="border-b px-4 py-2 text-left">Vehicle Info</th>
-            <th className="border-b px-4 py-2 text-left">Transaction Date</th>
-            <th className="border-b px-4 py-2 text-left">Amount</th>
-            <th className="border-b px-4 py-2 text-left">Method</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {filteredData.map((item, index) => (
-            <tr
-              key={index}
-              className={index % 2 === 0 ? "bg-white" : "bg-[#EEF4FF]"}
-            >
-              <td className="border-b px-4 py-2">
-                <Link
-                  href={`/estimate/view/${item.invoiceId}`}
-                  className="text-blue-500 hover:underline"
-                >
-                  {item.invoiceId}
-                </Link>
-              </td>
-              <td className="border-b px-4 py-2">
-                <Link
-                  href={`/client/${item.client.id}`}
-                  className="text-blue-500 hover:underline"
-                >
-                  {item.client.name}
-                </Link>
-              </td>
-              <td className="border-b px-4 py-2">{item.vehicle}</td>
-              <td className="border-b px-4 py-2">
-                {moment(item.date).format("YYYY-MM-DD")}
-              </td>
-              <td className="border-b px-4 py-2">${item.amount}</td>
-              <td className="border-b px-4 py-2">{item.method}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 }
