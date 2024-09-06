becbcad57e28346c385e0dc4959a6a344e4d1bd4
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-07 00:08:28 +0600

    backend of invoiceoneapp

--- src/app/settings/estimates/page.tsx
+++ src/app/settings/estimates/page.tsx
@@ -10,6 +10,22 @@ interface CurrencyData {
 
 export default function EstimateAndInvoicePage() {
   const [currencies, setCurrencies] = useState<{ value: string }[]>([]);
+  const [selectedCurrency, setSelectedCurrency] = useState<string>("USD");
+
+  const handleSubmitUpdate = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+
+    const tax = formData.get("taxAmount");
+    const terms = formData.get("terms");
+    const policy = formData.get("policy");
+
+    console.log("Form data1", tax);
+    console.log("Form data1", selectedCurrency);
+    console.log("Form data1", terms);
+    console.log("Form data1", policy);
+    // console.log("Form data",formData);
+  };
 
   useEffect(() => {
     fetch(
@@ -43,92 +59,95 @@ export default function EstimateAndInvoicePage() {
   }, []);
 
   const handleChange = (value: string) => {
-    console.log("Selected value:", value);
+    setSelectedCurrency(value);
   };
 
   return (
-    <div className="grid w-full grid-cols-2 items-start gap-4 px-5">
-      <div className="space-y-4">
-        {/* Currency & Tax */}
-        <div>
-          <h2 className="mb-2 text-xl font-semibold">Currency & Tax</h2>
-          <div className="space-y-3 rounded-sm border bg-white p-5">
-            <div className="flex items-center justify-evenly gap-2">
-              <SlimInput
-                name="taxAmount"
-                label="Tax Amount"
-                className="w-[320px]"
-              />
-              <div className="flex flex-col items-start">
-                <div className="mb-1 px-2 font-medium">Currency</div>
-                <Select
-                  showSearch
-                  defaultValue="USD"
-                  className="h-[30px] w-[320px]"
-                  filterOption={(input, option) =>
-                    (option?.value ?? " ")
-                      .toLowerCase()
-                      .includes(input.toLowerCase())
-                  }
-                  onChange={handleChange}
-                  options={currencies}
+    <form onSubmit={handleSubmitUpdate}>
+      <div className="grid w-full grid-cols-2 items-start gap-4 px-5">
+        <div className="space-y-4">
+          {/* Currency & Tax */}
+          <div>
+            <h2 className="mb-2 text-xl font-semibold">Currency & Tax</h2>
+            <div className="space-y-3 rounded-sm border bg-white p-5">
+              <div className="flex items-center justify-evenly gap-2">
+                <SlimInput
+                  name="taxAmount"
+                  label="Tax Amount"
+                  className="w-[320px]"
                 />
+                <div className="flex flex-col items-start">
+                  <div className="mb-1 px-2 font-medium">Currency</div>
+                  <Select
+                    showSearch
+                    defaultValue="USD"
+                    className="h-[30px] w-[320px]"
+                    filterOption={(input, option) =>
+                      (option?.value ?? " ")
+                        .toLowerCase()
+                        .includes(input.toLowerCase())
+                    }
+                    onChange={handleChange}
+                    options={currencies}
+                  />
+                </div>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="rounded-md bg-[#6571FF] px-10 py-1.5 text-white"
+                >
+                  Save
+                </button>
               </div>
             </div>
-            <div className="flex justify-end">
-              <button
-                type="button"
-                className="rounded-md bg-[#6571FF] px-10 py-1.5 text-white"
-              >
-                Save
-              </button>
+          </div>
+          {/* Terms & Conditions */}
+          <div>
+            <h2 className="mb-2 text-xl font-semibold">Terms & Conditions</h2>
+            <div className="space-y-3 rounded-sm border bg-white p-5">
+              <div className="grid grid-cols-2 items-start space-x-3">
+                <label className="block">
+                  <div className="mb-1 px-2 font-medium">
+                    Terms & Conditions
+                  </div>
+                  <textarea
+                    className="h-60 w-full resize-none rounded-sm border border-primary-foreground bg-white px-2 py-0.5 text-sm leading-6 outline-none"
+                    name="terms"
+                  />
+                </label>
+                <label className="block">
+                  <div className="mb-1 px-2 font-medium">Policy</div>
+                  <textarea
+                    className="h-60 w-full resize-none rounded-sm border border-primary-foreground bg-white px-2 py-0.5 text-sm leading-6 outline-none"
+                    name="policy"
+                  />
+                </label>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="rounded-md bg-[#6571FF] px-10 py-1.5 text-white"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Authorization */}
+          <div>
+            <h2 className="mb-2 text-xl font-semibold">Authorization</h2>
+            <div className="space-y-3 rounded-sm border bg-white p-5">
+              {/* TODO: future added */}
             </div>
           </div>
         </div>
-        {/* Terms & Conditions */}
-        <div>
-          <h2 className="mb-2 text-xl font-semibold">Terms & Conditions</h2>
-          <div className="space-y-3 rounded-sm border bg-white p-5">
-            <div className="grid grid-cols-2 items-start space-x-3">
-              <label className="block">
-                <div className="mb-1 px-2 font-medium">Terms & Conditions</div>
-                <textarea
-                  value='"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-'
-                  className="h-60 w-full resize-none rounded-sm border border-primary-foreground bg-white px-2 py-0.5 text-sm leading-6 outline-none"
-                />
-              </label>
-              <label className="block">
-                <div className="mb-1 px-2 font-medium">Policy</div>
-                <textarea
-                  value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
-                  className="h-60 w-full resize-none rounded-sm border border-primary-foreground bg-white px-2 py-0.5 text-sm leading-6 outline-none"
-                />
-              </label>
-            </div>
-            <div className="flex justify-end">
-              <button
-                type="button"
-                className="rounded-md bg-[#6571FF] px-10 py-1.5 text-white"
-              >
-                Save
-              </button>
-            </div>
-          </div>
-        </div>
-        {/* Authorization */}
-        <div>
-          <h2 className="mb-2 text-xl font-semibold">Authorization</h2>
-          <div className="space-y-3 rounded-sm border bg-white p-5">
-            {/* TODO: future added */}
-          </div>
-        </div>
-      </div>
 
-      {/* Email templates section */}
-      <div className="space-y-4 w-full">
-        <EmailTemplates />
+        {/* Email templates section */}
+        <div className="w-full space-y-4">
+          <EmailTemplates />
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
