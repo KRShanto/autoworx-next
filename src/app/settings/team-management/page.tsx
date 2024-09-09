ae3f28df64e33ecc446c9dada19e06a58546144e
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-09 12:15:54 +0600

    tmfirst started

--- src/app/settings/team-management/page.tsx
+++ src/app/settings/team-management/page.tsx
@@ -7,12 +7,10 @@ import UserComponent from "./UserComponent";
 export default function Page() {
   return (
     <div className="flex w-[100vw] gap-4">
-      {/* 1st Section */}
       <div className="w-[65%]">
         <UserRolesTable />
       </div>
 
-      {/* 2nd Section */}
       <div className="w-[35%]">
         <UserComponent />
       </div>
