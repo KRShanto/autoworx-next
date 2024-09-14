994b0d674b90d806604f75460ade8409ae4a77e4
KR Shanto <krshanto2005@gmail.com>
2024-09-14 12:51:50 +0600

    Refactor `getEmployees` function to exclude current user if specified

--- src/actions/employee/get.ts
+++ src/actions/employee/get.ts
@@ -1,13 +1,18 @@
 "use server";
+import { auth } from "@/app/auth";
 import { getCompanyId } from "@/lib/companyId";
 import { db } from "@/lib/db";
-// Get all the employees of a company
 
-export async function getEmployees() {
+// Get all the employees of a company
+export async function getEmployees({ excludeCurrentUser = false }) {
   const companyId = await getCompanyId();
+  const session = await auth();
   const employees = await db.user.findMany({
     where: {
       companyId,
+      id: {
+        not: excludeCurrentUser ? parseInt(session?.user?.id!) : undefined,
+      },
     },
   });
   return employees;
