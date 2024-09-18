4f87c202482db1e60e46fe4f7cf9b67f451f1a23
KR Shanto <krshanto2005@gmail.com>
2024-09-19 00:53:26 +0600

    log env variables in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@ import Title from "@/components/Title";
 import { auth } from "./auth";
 
 export default async function Page() {
-  const session = await auth();
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
+  console.log("AUTH_SECRET:", process.env.AUTH_SECRET);
+  console.log("PUSHER_ID:", process.env.PUSHER_ID);
+  console.log("PUSHER_KEY:", process.env.PUSHER_KEY);
+  console.log("PUSHER_SECRET:", process.env.PUSHER_SECRET);
+  console.log("PUSHER_CLUSTER:", process.env.PUSHER_CLUSTER);
+  console.log("NEXT_PUBLIC_PUSHER_KEY:", process.env.NEXT_PUBLIC_PUSHER_KEY);
+  console.log(
+    "NEXT_PUBLIC_PUSHER_CLUSTER:",
+    process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+  );
+  console.log("NEXT_PUBLIC_APP_URL:", process.env.NEXT_PUBLIC_APP_URL);
+  console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+  console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+  console.log("GMAIL_CLIENT_ID:", process.env.GMAIL_CLIENT_ID);
+  console.log("GMAIL_CLIENT_SECRET:", process.env.GMAIL_CLIENT_SECRET);
+  console.log("GMAIL_USER:", process.env.GMAIL_USER);
+  console.log("GMAIL_PASS:", process.env.GMAIL_PASS);
 
   return <Title>Dashboard</Title>;
 }
