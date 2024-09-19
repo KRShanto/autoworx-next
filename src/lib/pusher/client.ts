dd886b7a8ea82a34def3b2491fa42c527cadfe4b
KR Shanto <krshanto2005@gmail.com>
2024-09-19 11:57:34 +0600

    use `next-runtime-env` for getting public env variables in pusher route.

--- src/lib/pusher/client.ts
+++ src/lib/pusher/client.ts
@@ -1,7 +1,8 @@
 import Pusher from "pusher-js";
+import { env } from "next-runtime-env";
 
-console.log("Pusher key (client): ", process.env.NEXT_PUBLIC_PUSHER_KEY);
+console.log("Pusher key (client): ", env("NEXT_PUBLIC_PUSHER_KEY"));
 
-export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+export const pusher = new Pusher(env("NEXT_PUBLIC_PUSHER_KEY") as string, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER as string,
 });
