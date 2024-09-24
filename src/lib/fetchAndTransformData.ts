49f852f01a354764189f2c52b0f3830041e08f99
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-24 13:19:43 +0600

    modified the estimate structure::initial

---
+++ src/lib/fetchAndTransformData.ts
@@ -0,0 +1,55 @@
+import { db } from "@/lib/db";
+import { InvoiceType } from "@prisma/client";
+
+export async function fetchAndTransformData(
+  type: InvoiceType,
+  companyId: number,
+  searchParams: { startDate?: string; endDate?: string; status?: string }
+) {
+  const { startDate, endDate, status } = searchParams;
+
+  const data = await db.invoice.findMany({
+    where: {
+      type,
+      companyId,
+      createdAt: {
+        gte: startDate ? new Date(`${startDate}T00:00:00`) : undefined,
+        lte: endDate ? new Date(`${endDate}T23:59:59.999`) : undefined,
+      },
+      statusId: status ? parseInt(status) : undefined,
+    },
+  });
+
+  return await Promise.all(
+    data.map(async (item) => {
+      const vehicle = item.vehicleId
+        ? await db.vehicle.findFirst({
+            where: { id: item.vehicleId },
+          })
+        : null;
+      const client = item.clientId
+        ? await db.client.findFirst({
+            where: { id: item.clientId },
+          })
+        : null;
+      const status = item.statusId
+        ? await db.status.findFirst({
+            where: { id: item.statusId },
+          })
+        : null;
+
+      return {
+        id: item.id,
+        clientName: client?.firstName + " " + client?.lastName || "",
+        vehicle: vehicle?.model || "",
+        email: client?.email || "",
+        phone: client?.mobile || "",
+        grandTotal: item.grandTotal as any,
+        createdAt: item.createdAt,
+        status: status?.name || "",
+        textColor: status?.textColor || "",
+        bgColor: status?.bgColor || "",
+      };
+    })
+  );
+}
