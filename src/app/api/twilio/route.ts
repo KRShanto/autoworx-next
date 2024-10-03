be80f0b297fbfabfbc19a4e40185abc0755ccf7d
KR Shanto <krshanto2005@gmail.com>
2024-10-03 10:00:55 +0600

    ignore the twilio route for now.

--- src/app/api/twilio/route.ts
+++ src/app/api/twilio/route.ts
@@ -1,24 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import twilio from "twilio";
 
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_TOKEN;
-const from = process.env.TWILIO_NUMBER;
-const client = twilio(accountSid, authToken);
+// const accountSid = process.env.TWILIO_SID;
+// const authToken = process.env.TWILIO_TOKEN;
+// const from = process.env.TWILIO_NUMBER;
+// const client = twilio(accountSid, authToken);
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const { to, message } = body;
+// export async function POST(req: NextRequest) {
+//   const body = await req.json();
+//   const { to, message } = body;
 
-  try {
-    await client.messages.create({
-      body: message,
-      from,
-      to,
-    });
-    return NextResponse.json({ status: 200, body: "Message sent" });
-  } catch (error) {
-    console.log("Error sending message", error);
-    return NextResponse.json({ status: 500, body: "Error sending message" });
-  }
+//   try {
+//     await client.messages.create({
+//       body: message,
+//       from,
+//       to,
+//     });
+//     return NextResponse.json({ status: 200, body: "Message sent" });
+//   } catch (error) {
+//     console.log("Error sending message", error);
+//     return NextResponse.json({ status: 500, body: "Error sending message" });
+//   }
+// }
+
+export async function POST() {
+  return NextResponse.json({ status: 200, body: "Message sent" });
 }
