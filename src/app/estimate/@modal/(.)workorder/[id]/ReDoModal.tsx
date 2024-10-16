4ef381b5c4dc5c4a32b1b4e0531a99f9f9b18472
rayhan <rayhanmujumdar0177@gmail.com>
2024-10-16 17:40:55 +0600

    refactor code for re-do modal

--- src/app/estimate/@modal/(.)workorder/[id]/ReDoModal.tsx
+++ src/app/estimate/@modal/(.)workorder/[id]/ReDoModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   DialogClose,
+  DialogContent,
   DialogContentBlank,
   DialogFooter,
   DialogOverlay,
@@ -22,134 +23,123 @@ export default function ReDoModal() {
         Re-Do
       </button>
       <Dialog open={open} onOpenChange={() => setOpen((prev) => !prev)}>
-        <div>
-          <DialogPortal>
-            <DialogOverlay />
-            <DialogContentBlank className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 flex max-h-full translate-x-[-50%] translate-y-[-50%] justify-center gap-4 duration-200">
-              {/* content */}
-              <div className="min-h-80 min-w-[500px] space-y-3 rounded-md bg-white">
-                <div className="mx-10 my-5">
-                  <div>
-                    <h3 className="text-xl font-bold">Service 1</h3>
-                    <p className="text-semibold text-base">
-                      Select employee for re-do
-                    </p>
+        <DialogContent>
+          <div className="space-y-3 rounded-md bg-white">
+            <div className="mx-10 my-5">
+              <div>
+                <h3 className="text-xl font-bold">Service 1</h3>
+                <p className="text-semibold text-base">
+                  Select employee for re-do
+                </p>
+              </div>
+              <div className="mt-5 flex flex-col justify-center space-y-1">
+                <div className="flex">
+                  <p className="min-w-[150px] text-center">Name</p>
+                  <p>Notes</p>
+                </div>
+                <div className="space-y-3">
+                  {/* input - 1 */}
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="john"
+                      className="flex min-w-[150px] items-center justify-start space-x-3"
+                    >
+                      <input
+                        className="size-4 shrink-0 accent-[#6571FF]"
+                        type="checkbox"
+                        name="john"
+                        id="john"
+                      />
+                      <p id="john" className="cursor-pointer text-center">
+                        John Smith
+                      </p>
+                    </label>
+                    <input
+                      className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
+                      type="text"
+                      name=""
+                      id=""
+                    />
                   </div>
-                  <div className="mt-5 flex flex-col justify-center space-y-1">
-                    <div className="flex">
-                      <p className="min-w-[150px] text-center">Name</p>
-                      <p>Notes</p>
-                    </div>
-                    <div className="space-y-3">
-                      {/* input - 1 */}
-                      <div className="flex items-center">
-                        <label
-                          htmlFor="john"
-                          className="flex min-w-[150px] items-center justify-start space-x-3"
-                        >
-                          <input
-                            className="size-4 shrink-0 accent-[#6571FF]"
-                            type="checkbox"
-                            name="john"
-                            id="john"
-                          />
-                          <p id="john" className="cursor-pointer text-center">
-                            John Smith
-                          </p>
-                        </label>
-                        <input
-                          className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
-                          type="text"
-                          name=""
-                          id=""
-                        />
-                      </div>
-                      {/* input - 2 */}
-                      <div className="flex items-center">
-                        <label
-                          htmlFor="Tim Smith"
-                          className="flex min-w-[150px] items-center justify-start space-x-3"
-                        >
-                          <input
-                            className="size-4 shrink-0 accent-[#6571FF]"
-                            type="checkbox"
-                            name="john"
-                            id="Tim Smith"
-                          />
-                          <p id="john" className="cursor-pointer text-center">
-                            Tim Smith
-                          </p>
-                        </label>
-                        <input
-                          className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
-                          type="text"
-                          name=""
-                          id=""
-                        />
-                      </div>
-                      {/* input - 3 */}
-                      <div className="flex items-center">
-                        <label
-                          htmlFor="Dan Smith"
-                          className="flex min-w-[150px] items-center justify-start space-x-3"
-                        >
-                          <input
-                            className="size-4 shrink-0 accent-[#6571FF]"
-                            type="checkbox"
-                            name="john"
-                            id="Dan Smith"
-                          />
-                          <p id="john" className="cursor-pointer text-center">
-                            Dan Smith
-                          </p>
-                        </label>
-                        <input
-                          className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
-                          type="text"
-                          name=""
-                          id=""
-                        />
-                      </div>
-                      {/* input - 4 */}
-                      <div className="flex items-center">
-                        <label
-                          htmlFor="Harry Smith"
-                          className="flex min-w-[150px] items-center justify-start space-x-3"
-                        >
-                          <input
-                            className="size-4 shrink-0 accent-[#6571FF]"
-                            type="checkbox"
-                            name="john"
-                            id="Harry Smith"
-                          />
-                          <p id="john" className="cursor-pointer text-center">
-                            Harry Smith
-                          </p>
-                        </label>
-                        <input
-                          className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
-                          type="text"
-                          name=""
-                          id=""
-                        />
-                      </div>
-                    </div>
+                  {/* input - 2 */}
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="Tim Smith"
+                      className="flex min-w-[150px] items-center justify-start space-x-3"
+                    >
+                      <input
+                        className="size-4 shrink-0 accent-[#6571FF]"
+                        type="checkbox"
+                        name="john"
+                        id="Tim Smith"
+                      />
+                      <p id="john" className="cursor-pointer text-center">
+                        Tim Smith
+                      </p>
+                    </label>
+                    <input
+                      className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
+                      type="text"
+                      name=""
+                      id=""
+                    />
+                  </div>
+                  {/* input - 3 */}
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="Dan Smith"
+                      className="flex min-w-[150px] items-center justify-start space-x-3"
+                    >
+                      <input
+                        className="size-4 shrink-0 accent-[#6571FF]"
+                        type="checkbox"
+                        name="john"
+                        id="Dan Smith"
+                      />
+                      <p id="john" className="cursor-pointer text-center">
+                        Dan Smith
+                      </p>
+                    </label>
+                    <input
+                      className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
+                      type="text"
+                      name=""
+                      id=""
+                    />
+                  </div>
+                  {/* input - 4 */}
+                  <div className="flex items-center">
+                    <label
+                      htmlFor="Harry Smith"
+                      className="flex min-w-[150px] items-center justify-start space-x-3"
+                    >
+                      <input
+                        className="size-4 shrink-0 accent-[#6571FF]"
+                        type="checkbox"
+                        name="john"
+                        id="Harry Smith"
+                      />
+                      <p id="john" className="cursor-pointer text-center">
+                        Harry Smith
+                      </p>
+                    </label>
+                    <input
+                      className="w-full rounded-sm border border-gray-500 pl-1 focus:outline-none"
+                      type="text"
+                      name=""
+                      id=""
+                    />
                   </div>
                 </div>
-                <DialogFooter className="py-8">
-                  <button className="mx-auto rounded bg-[#6571FF] px-8 py-2 text-white">
-                    Save Changes
-                  </button>
-                </DialogFooter>
               </div>
-
-              <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground print:hidden">
-                <HiXMark className="h-6 w-6 text-slate-500" />
-                <span className="sr-only">Close</span>
-              </DialogClose>
-            </DialogContentBlank>
-          </DialogPortal>
-        </div>
+            </div>
+            <DialogFooter className="py-8">
+              <button className="mx-auto rounded bg-[#6571FF] px-8 py-2 text-white">
+                Save Changes
+              </button>
+            </DialogFooter>
+          </div>
+        </DialogContent>
       </Dialog>
     </>
   );
