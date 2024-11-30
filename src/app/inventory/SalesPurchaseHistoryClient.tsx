59d9510da4412a79e76e1c2ad79363dcf55bc696
Sunzim <sunxiiim@gmail.com>
2024-11-30 06:30:05 +0600

    fix: change Sales List to Use List

--- src/app/inventory/SalesPurchaseHistoryClient.tsx
+++ src/app/inventory/SalesPurchaseHistoryClient.tsx
@@ -54,7 +54,7 @@ export default function SalesPurchaseHistoryClient({
                 : "border border-[#6571FF] text-[#6571FF]",
             )}
           >
-            Sales List
+            Use List
           </Tabs.Trigger>
           <Tabs.Trigger
             value={Tab.Purchase}
