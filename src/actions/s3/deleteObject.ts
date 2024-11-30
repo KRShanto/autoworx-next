dc7bd43ef90956c59b6429d52cfb4c26449bafee
KR Shanto <krshanto2005@gmail.com>
2024-11-30 16:34:27 +0600

    Implement S3 file upload and deletion functionality with signed URLs

---
+++ src/actions/s3/deleteObject.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { s3Client } from "@/lib/s3Client";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+export async function deleteObject(url: string) {
+  try {
+    const key = url.split("/").slice(-1)[0];
+
+    const deleteParams = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: key,
+    };
+
+    await s3Client.send(new DeleteObjectCommand(deleteParams));
+  } catch (e) {
+    console.error(e);
+  }
+}
---
+++ src/actions/s3/signedURL.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { auth } from "@/app/auth";
+import { s3Client } from "@/lib/s3Client";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from "crypto";
+
+// 50mb
+const maxFileSize = 50 * 1024 * 1024;
+
+// generate a random file name
+const generateFileName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
+
+type SignedURLResponse = Promise<
+  | { error?: undefined; success: { url: string } }
+  | { error: string; success?: undefined }
+>;
+
+type GetSignedURLParams = {
+  fileType: string;
+  fileSize: number;
+  checksum: string;
+};
+
+export async function getSignedURL({
+  fileType,
+  fileSize,
+  checksum,
+}: GetSignedURLParams): Promise<SignedURLResponse> {
+  const session = await auth();
+
+  if (!session) {
+    return { error: "not authenticated" };
+  }
+
+  if (fileSize > maxFileSize) {
+    return { error: "File size too large" };
+  }
+
+  const fileName = generateFileName();
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: fileName,
+    ContentType: fileType,
+    ContentLength: fileSize,
+    ChecksumSHA256: checksum,
+  });
+
+  const url = await getSignedUrl(
+    s3Client,
+    putObjectCommand,
+    { expiresIn: 60 }, // 60 seconds
+  );
+
+  console.log({ success: url });
+
+  return { success: { url } };
+}
