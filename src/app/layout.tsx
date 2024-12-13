1663ac6307ed83164771854ba3dde9d3748410dc
Sunzim <sunxiiim@gmail.com>
2024-12-13 15:52:32 +0600

    fix issues (check description of PR)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import Layout from "@/components/Layout";
 import { TooltipProvider } from "@/components/Tooltip";
+import { EmployeeType } from "@prisma/client";
 import type { Metadata } from "next";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { PublicEnvScript } from "next-runtime-env";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import TopLoader from "../components/TopLoader";
 import { auth } from "./auth";
 import "./globals.css";
-import { Toaster } from "react-hot-toast";
-import { PublicEnvScript } from "next-runtime-env";
-import { Session } from "next-auth";
-import { EmployeeType } from "@prisma/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,7 +36,22 @@ export default async function RootLayout({
       </head>
       <body className={inter.className}>
         <TopLoader />
-        <Toaster position="top-right" reverseOrder={false} />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            success: {
+              style: {
+                border: "1px solid rgba(0, 255, 0, 0.5)",
+              },
+            },
+            error: {
+              style: {
+                border: "1px solid rgba(255, 0, 0, 0.5)",
+              },
+            },
+          }}
+        />
         <SessionProvider session={session}>
           <TooltipProvider delayDuration={150}>
             <Layout session={session}>{children}</Layout>
