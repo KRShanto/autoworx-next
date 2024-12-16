2f9542a7f29dc4e6bdda26ebdd1261ea8f63b3f1
Sunzim <sunxiiim@gmail.com>
2024-12-16 19:43:46 +0600

    test twilio sms receive with zapier

--- src/app/api/twilio/route.ts
+++ src/app/api/twilio/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import twilio from "twilio";
 
@@ -23,6 +24,48 @@ import twilio from "twilio";
 //   }
 // }
 
-export async function POST() {
-  return NextResponse.json({ status: 200, body: "Message sent" });
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    await db.twilioMessage.create({
+      data: {
+        from: body.from,
+        to: body.to,
+        message: body.body,
+        timestamp: body.timestamp,
+        companyId: 1,
+      },
+    });
+    console.log("Webhook Subscription Data:", body);
+
+    // Send response
+    return Response.json(
+      { message: "Webhook subscription successful", data: body },
+      { status: 200 },
+    );
+  } catch (error: any) {
+    console.error("Subscription error:", error);
+    return Response.json(
+      { message: "Webhook subscription failed", error: error?.message },
+      { status: 500 },
+    );
+  }
+}
+export async function DELETE(req: NextRequest) {
+  try {
+    const body = await req.json();
+    console.log("Webhook Unsubscribe Data:", body);
+
+    // Send response
+    return Response.json(
+      { message: "Webhook unsubscribed successfully", data: body },
+      { status: 200 },
+    );
+  } catch (error: unknown) {
+    console.error("Unsubscribe error:", error);
+    return Response.json(
+      { message: "Webhook unsubscribe failed", error: error?.message },
+      { status: 500 },
+    );
+  }
 }
