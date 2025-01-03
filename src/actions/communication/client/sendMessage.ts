c9d507e031bdc1aeef9846b1a7a305cb3c749376
KR Shanto <krshanto2005@gmail.com>
2025-01-03 18:51:31 +0600

    feat: enhance deploy workflow with conditional actions and automatic merging to production

--- src/actions/communication/client/sendMessage.ts
+++ src/actions/communication/client/sendMessage.ts
@@ -5,11 +5,8 @@ import { env } from "next-runtime-env";
 import Twilio from "twilio";
 
 const accountSid = env("TWILIO_SID") || process.env.TWILIO_SID;
-console.log("🚀 ~ accountSid:", accountSid);
 const authToken = env("TWILIO_TOKEN") || process.env.TWILIO_TOKEN;
-console.log("🚀 ~ authToken:", authToken);
 const fromNumber = env("TWILIO_NUMBER") || process.env.TWILIO_NUMBER;
-console.log("🚀 ~ fromNumber:", fromNumber);
 
 const twilio = Twilio(accountSid, authToken);
 
